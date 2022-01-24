82117ebd56edafab136b3a7bb11035c9d64dcb64
Volodymyr Klochan <vovkis13@gmail.com>
2022-01-24 19:01:43 +0200

    initial

---
+++ src/components/App/App.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from 'react-redux';
+import { BallTriangle } from 'react-loader-spinner';
+import { getLoading, getError } from 'redux/selectors';
+import ContactForm from 'components/ContactForm';
+import Filter from 'components/Filter';
+import ContactList from 'components/ContactList/';
+
+export default function App() {
+  const isLoading = useSelector(getLoading);
+  const errorMsg = useSelector(getError);
+  return (
+    <div>
+      <h1>Phonebook (ASYNC REDUX RTKQuery)</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      {isLoading && <BallTriangle color="#ffaa00" height={80} width={80} />}
+      <ContactList />
+      {errorMsg && <p>{errorMsg}</p>}
+    </div>
+  );
+}
